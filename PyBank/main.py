f58f1247043359a4bead7316916afb341bf9ca89
sjryandesigns <sjryandesigns@gmail.com>
2020-10-10 02:04:30 -0700

    State Save

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,44 +1,50 @@
-# #Import modules os to create file paths and csv to read CSV files
-# import os
-# import csv
+#Import modules os to create file paths and csv to read CSV files
+import os
+import csv
 
-# #Create file path to csv source file
-# budget_csv = os.path.join(os.path.dirname(__file__), "Resources", "budget_data.csv")
+#Create file path to csv source file
+budget_csv = os.path.join(os.path.dirname(__file__), "Resources", "budget_data.csv")
 
 
-# # def budget_analysis(budget_data):
+def budget_analysis(budget_data):
     
-#     # date = str(budget_data[0])
-#     # profit = budget_data[1]
-#     # next_profit = next(profit)
-
-#     # total_months = len(date)
-#     # net_profit = sum(profit)
-#     # avg_change = (net_profit / total_months)
+    date = str(budget_data[0])
+    # profit = int(budget_data[1])
+    # next_profit=0
     
-#     # if profit > next_profit:
-#     #     increase_total = profit
-#     #     increase_date = date
-#     # if profit > next_profit:
-#     #     decrease_total = profit
-#     #     decrease_date = date
 
-#     # print("Financial Analysis")
-#     # print("----------------------------")
-#     # print(f"Total Months: {str(total_months)}")
-#     # print(f"Total: {str(net_profit)}")
-#     # print(f"Average Change: {str(avg_change)}")
-#     # print(f"Greatest Increase in Profits: {increase_date} {str((increase_total))}")
-#     # print(f"Greatest Decrease in Profits: {decrease_date} {str((decrease_total))}")
-
-# # Open and read csv
-# with open(budget_csv) as csv_file:
-#     #Specify delimiter and variable that holds contents
-#     csv_reader = csv.reader(csv_file, delimiter=",")
-
-#     # Read the header row first 
-#     header = next(csv_file)
+    total_months = len(budget_data[0])
+    # net_profit = sum(profit)
+    # avg_change = (net_profit / total_months)
     
+    # for rows in csv_reader:
+       
+    #     if profit > next_profit:
+    #         increase_total = profit
+    #         increase_date = date
+    #     if profit > next_profit:
+    #         decrease_total = profit
+    #         decrease_date = date
+
+    print("Financial Analysis")
+    print("----------------------------")
+    print(f'{date}')
+    print(f"Total Months: {total_months}")
+    # print(f"Total: {net_profit}")
+    # # print(f"Average Change: {avg_change}")
+    # print("Greatest Increase in Profits: " + str(increase_total) + str(increase_profit))
+    # print(f"Greatest Decrease in Profits: {decrease_date} -({increase_total})")
+
+# Open and read csv
+with open(budget_csv) as csv_file:
+    #Specify delimiter and variable that holds contents
+    csv_reader = csv.reader(csv_file, delimiter=",")
+
+    # Read the header row first 
+    
+    header = next(csv_file)
+    budget_analysis(budget_csv)
+
 #     #Read each row of data after header
 #     for rows in csv_reader:
 #         date = budget_csv[0]
