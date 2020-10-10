52b840da57ff655a13383389590f23bf262025f7
sjryandesigns <sjryandesigns@gmail.com>
2020-10-10 00:00:40 -0700

    Adding function back in

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,32 @@
 # #Create file path to csv source file
 # budget_csv = os.path.join(os.path.dirname(__file__), "Resources", "budget_data.csv")
 
+
+# # def budget_analysis(budget_data):
+    
+#     # date = str(budget_data[0])
+#     # profit = budget_data[1]
+#     # next_profit = next(profit)
+
+#     # total_months = len(date)
+#     # net_profit = sum(profit)
+#     # avg_change = (net_profit / total_months)
+    
+#     # if profit > next_profit:
+#     #     increase_total = profit
+#     #     increase_date = date
+#     # if profit > next_profit:
+#     #     decrease_total = profit
+#     #     decrease_date = date
+
+#     # print("Financial Analysis")
+#     # print("----------------------------")
+#     # print(f"Total Months: {str(total_months)}")
+#     # print(f"Total: {str(net_profit)}")
+#     # print(f"Average Change: {str(avg_change)}")
+#     # print(f"Greatest Increase in Profits: {increase_date} {str((increase_total))}")
+#     # print(f"Greatest Decrease in Profits: {decrease_date} {str((decrease_total))}")
+
 # # Open and read csv
 # with open(budget_csv) as csv_file:
 #     #Specify delimiter and variable that holds contents
