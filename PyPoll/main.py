9ec25de607890188a15932c878e2686ba973ef77
sjryandesigns <sjryandesigns@gmail.com>
2020-10-10 00:32:25 -0700

    Poll - setup csv read

---
+++ PyPoll/main.py
@@ -0,0 +1,26 @@
+#Import modules os to create file paths and csv to read CSV files
+import os
+import csv
+
+#Create file path to csv source file
+poll_csv = os.path.join(os.path.dirname(__file__), "Resources", "election_data.csv")
+
+
+# Open and read csv
+with open(poll_csv) as csv_file:
+    #Specify delimiter and variable that holds contents
+    csv_reader = csv.reader(csv_file, delimiter=",")
+
+    # Read the header row first 
+    header = next(csv_file)
+    
+    #Read each row of data after header
+    results={
+        "VoterID":poll_csv[0], 
+        "County":poll_csv[1],
+        "Candidate":poll_csv[2]
+    }
+
+    print("Election Results")
+    print("-------------------------")
+    print(f'"Total Votes: "+{results(len(["VoterID"]))}')
