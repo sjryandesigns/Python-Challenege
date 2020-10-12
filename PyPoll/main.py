4d6ad94545b2d99ec63a335270284ee3c5f51f4f
sjryandesigns <sjryandesigns@gmail.com>
2020-10-12 01:19:05 -0700

    Post office hours code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,14 +8,32 @@ poll_csv = os.path.join(os.path.dirname(__file__), "Resources", "election_data.c
 
 # Open and read csv
 with open(poll_csv) as csv_file:
-    #Specify delimiter and variable that holds contents
+    # Specify delimiter and variable that holds contents
     csv_reader = csv.reader(csv_file, delimiter=",")
 
     # Read the header row first 
     header = next(csv_file)
     
-    #Read each row of data after header
+    total_votes = 0
+    all_candidates_w_dup = []
+    candidate_totals = {}
+
+    for rows in csv_reader:
+        total_votes += 1
+
+        all_candidates_w_dup.append(rows[2])
+
+        if candidate_totals.get(rows[2]):
+            candidate_totals[rows[2]]+=1
+        else:
+            candidate_totals[rows[2]]=1
+        
+
+    print(candidates_totals)
+
     
-    print("Election Results")
-    print("-------------------------")
+    # print("Election Results")
+    # print("-------------------------")
+    # print(f"Total Votes: {total_votes}")
+
     
